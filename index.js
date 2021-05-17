f68a34baee627f36ce2543b453ece782135174b9
Rusu0221 <83679117+Rusu0221@users.noreply.github.com>
2021-05-17 21:56:20 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,27 +5,27 @@ var score = 0, round = 0, double = 0;
 function button(x) {
 	if (random == x) {
 		txt.innerHTML = "Win";
-  	txt.style.color = "green";
-    random = Math.floor(Math.random() * 3) + 1;
-    score += 1 + double;
-    ++double;
-    document.getElementById("score").innerHTML = "Score " + score;
-    } else {
-    	txt.innerHTML = "Lose";
-      txt.style.color = "red";
-      document.getElementById("1").disabled = true;
+  		txt.style.color = "green";
+   		random = Math.floor(Math.random() * 3) + 1;
+		score += 1 + double;
+    		++double;
+    		document.getElementById("score").innerHTML = "Score " + score;
+    	} else {
+    		txt.innerHTML = "Lose";
+     		txt.style.color = "red";
+      		document.getElementById("1").disabled = true;
  	  	document.getElementById("2").disabled = true;
-    	document.getElementById("3").disabled = true;
-      double = 0;
-    }
-    ++round;
-    document.getElementById("round").innerHTML = "Round " + round;
+    		document.getElementById("3").disabled = true;
+      		double = 0;
+    	}
+    	++round;
+    	document.getElementById("round").innerHTML = "Round " + round;
 }
 
 function restart() {
-		document.getElementById("1").disabled = false;
-    document.getElementById("2").disabled = false;
-    document.getElementById("3").disabled = false;
-    txt.innerHTML = "";
-    random = Math.floor(Math.random() * 3) + 1;
+	document.getElementById("1").disabled = false;
+    	document.getElementById("2").disabled = false;
+    	document.getElementById("3").disabled = false;
+    	txt.innerHTML = "";
+    	random = Math.floor(Math.random() * 3) + 1;
 }
